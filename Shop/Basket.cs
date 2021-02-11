4e7ac66a4d9cd74fcc6eaf4ab25dbac544f75e57
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-02-11 20:31:10 +0200

    Basket

--- Shop/Basket.cs
+++ Shop/Basket.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Shop
 {
+    // Class for users's basket    
     class Basket
     {
+        public List<Item> Items { get; set; }
+
+        // In the constructor empty list is created
+        public Basket()
+        {
+            Items = new List<Item>();
+        }
+
+        public decimal CalculateTotalPrice()
+        {
+            /*
+            decimal total = 0;
+            foreach(var item in Items)
+            {
+                total += item.Price;
+            }
+
+            return total;
+            */
+
+            // OR:
+            return Items.Sum(i => i.Price);
+        }
     }
 }
--- Shop/ShopManager.cs
+++ Shop/ShopManager.cs
@@ -4,7 +4,42 @@ using System.Text;
 
 namespace Shop
 {
+    // Class for the logic
     class ShopManager
     {
+        public Basket Basket { get; set; }
+        public Storage Storage { get; set; }
+
+        // In the constructor new basket and new storage are created
+        public ShopManager()
+        {
+            Basket = new Basket();
+            Storage = new Storage();
+        }
+
+        // method returns all the items inside Storage, sorted by the name 
+        public List<Item> GetAvailableItems()
+        {
+
+        }
+
+        // method check if item by this name exists in the storage. 
+        // If not - returns null, otherwise adds item to the basket and returns it as result.
+        public Item AddToBasket(string name)
+        {
+
+        }
+
+        // Returns total price of all items in the basket
+        public decimal CheckOut()
+        {
+
+        }
+
+        // Returns all items in the basket, sorted by the price 
+        public List<Item> Pay()
+        {
+
+        }
     }
 }
