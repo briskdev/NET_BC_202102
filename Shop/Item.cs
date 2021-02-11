57e676e93977e86c40bb0bff7bc2e7cc1c663b46
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-02-11 19:50:29 +0200

    Shop

---
+++ Shop/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+    // Class describes one item
+    class Item
+    {
+        public decimal Price { get; set; }
+
+        public string Name { get; set; }
+
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Item()
+        {
+
+        }
+    }
+}
---
+++ Shop/Storage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+    // Class for available items (catalog)
+    class Storage
+    {
+        public List<Item> Items { get; set; }
+
+        // In the constructor list is filled with predefined items
+        public Storage()
+        {
+            Items = new List<Item>();
+            Items.Add(new Item("PC", 900));
+            Items.Add(new Item("Pants", 50));
+            Items.Add(new Item("Car", 10000));
+            Items.Add(new Item("Mobile", 200));
+            Items.Add(new Item("TV", 550));
+            Items.Add(new Item("Glasses", 100));
+            Items.Add(new Item("Gloves", 20));
+            Items.Add(new Item("Hat", 10));
+        }
+    }
+}
