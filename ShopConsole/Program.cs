a301b693c63f50ec9128f3868312b15ff0592984
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-02-17 19:29:13 +0200

    Refactoring

--- ShopConsole/Program.cs
+++ ShopConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using Shop;
 
-namespace Shop
+namespace ShopConsole
 {
     class Program
     {
@@ -10,24 +11,24 @@ namespace Shop
 
             // items in the storage
             Console.WriteLine("Items available:");
-            foreach(var item in shop.GetAvailableItems())
+            foreach (var item in shop.GetAvailableItems())
             {
                 Console.WriteLine("{0}: EUR {1}", item.Name, item.Price);
             }
 
             // begin the shopping process
-            while(true)
+            while (true)
             {
                 Console.Write("Enter item's name: ");
                 string input = Console.ReadLine();
-                if(input == "0")
+                if (input == "0")
                 {
                     // stop the shopping process
                     break;
                 }
 
                 var itemAdded = shop.AddToBasket(input);
-                if(itemAdded == null)
+                if (itemAdded == null)
                 {
                     Console.WriteLine("Item is not available!");
                 }
@@ -39,7 +40,7 @@ namespace Shop
 
             Console.WriteLine("Total price: EUR {0}", shop.CheckOut());
             Console.WriteLine("Your purchase:");
-            foreach(var item in shop.Pay())
+            foreach (var item in shop.Pay())
             {
                 Console.WriteLine("{0}: EUR {1}", item.Name, item.Price);
             }
