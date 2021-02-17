8a5b83288028839a27571d1b68152b49874951db
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-02-17 20:13:10 +0200

    Library structure

---
+++ Library.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
