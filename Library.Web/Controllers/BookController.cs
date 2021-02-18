1d44a8fabcbddbcdb2c13d901e4d1a4833800c1e
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-02-18 19:53:47 +0200

    Library.Web: Book list

---
+++ Library.Web/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Library.Logic.Managers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private static BookManager manager = new BookManager();
+
+        public IActionResult Index()
+        {
+            var books = manager.GetAvailableBooks();
+
+            return View(books);
+        }
+    }
+}
