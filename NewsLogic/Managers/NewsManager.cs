044c17ce99c2bc2f9b150ac01ac4bfe4a99e92c7
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-02-25 21:10:33 +0200

    NewsLogic: Structure

---
+++ NewsLogic/Managers/NewsManager.cs
@@ -0,0 +1,16 @@
+﻿using NewsLogic.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsLogic.Managers
+{
+    public class NewsManager
+    {
+        public List<Article> GetLatestNews(int count = 6)
+        {
+            // TODO: return latest 'count' news ordered by date
+            throw new NotImplementedException();
+        }
+    }
+}
