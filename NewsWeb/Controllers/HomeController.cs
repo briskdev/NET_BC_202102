e4447edd78fe8e58dd8101429e7ad06e4646a91b
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-02-25 20:59:56 +0200

    NewsWeb: Layout

---
+++ NewsWeb/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ NewsWeb/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsWeb.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Topics()
+        {
+            return View();
+        }
+
+        public IActionResult Article()
+        {
+            return View();
+        }
+    }
+}
