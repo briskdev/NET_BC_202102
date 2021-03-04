ccc52a5707e0045548ed67e4984ae8257ef2fd70
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-03-04 21:20:32 +0200

    NewsConsole

---
+++ NewsConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using NewsLogic.Managers;
+using System;
+
+namespace NewsConsole
+{
+    class Program
+    {
+        private static NewsManager news = new NewsManager();
+        private static TopicManager topics = new TopicManager();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Topics: ");
+            topics.GetAllTopics().ForEach(t =>
+            {
+                Console.WriteLine(t.Title);
+            });
+
+            Console.WriteLine("Articles: ");
+            news.GetLatestNews().ForEach(a =>
+            {
+                Console.WriteLine(a.Title);
+            });
+        }
+    }
+}
