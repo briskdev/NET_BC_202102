acebaf81a477cb0e1c0e98cb7a13f1777940cfe9
Kristaps Briška <kristaps.briska@rixtech.lv>
2021-03-24 20:37:53 +0200

    Asp.Net Core WEB Api

---
+++ MyApi/Controllers/EmployeesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] // http://localhost/employees
+    public class EmployeesController : ControllerBase
+    {
+        private static List<Employee> employees = new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                Name = "John",
+                Surname ="Smith",
+                Department = "IT"
+            },
+            new Employee()
+            {
+                Id = 2,
+                Name = "Ann",
+                Surname = "Addams",
+                Department = "Marketing"
+            }
+        };
+
+        // http://localhost/employees
+        [HttpGet]
+        public List<Employee> Get()
+        {
+            return employees.OrderBy(e => e.Surname).ToList();
+        }
+
+        // http://localhost/employees/{id}
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return employees.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
---
+++ MyApi/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApi
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Department { get; set; }
+    }
+}
